852142b91ac1f1879d5852b7c598a5d651720c52
pvchine <pvchinbn@yahoo.com>
2021-06-28 09:08:33 +0800

    Jun 28 second

--- functions/dailyallowancesTable/createDailyAllowsDetl.js
+++ functions/dailyallowancesTable/createDailyAllowsDetl.js
@@ -4,7 +4,7 @@ module.exports = async (event) => {
   const { ...fields } = JSON.parse(event.body);
   try {
     const createdDailyAllowsDetl = await table.create([{ fields }]);
-    return formattedReturn(200, createdDailyAllowDetl);
+    return formattedReturn(200, createdDailyAllowsDetl);
   } catch (err) {
     console.error(err);
     return formattedReturn(500, {});
--- functions/dailyallowancesTable/deleteDailyAllowsDetl.js
+++ functions/dailyallowancesTable/deleteDailyAllowsDetl.js
@@ -3,7 +3,7 @@ const formattedReturn = require("../formattedReturn");
 module.exports = async (event) => {
   const { id } = JSON.parse(event.body);
   try {
-    const deletedDailyAllowDetl = await table.destroy(id);
+    const deletedDailyAllowsDetl = await table.destroy(id);
     return formattedReturn(200, deletedDailyAllowsDetl);
   } catch (err) {
     console.error(err);
